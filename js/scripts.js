89f35348dcf2c51674ebe9da5c5b572d996bb6c7
Beth Hansen and Kimberly Lu <beth@>
2017-07-05 14:00:29 -0700

    add event handler to the first two terms

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
-  $(".clickable").click(function() {
-    $(".showing").show();
+$(document).ready(function(){ //reaches out to juery and when/where to load it
+  $ ("#javascript").click(function() {//jquery is attaching the click event handler to the specific ID
+    $("#javascript .titleCard").toggle();//when clicked this will turn off titlecard
+    $("#javascript .definition").toggle();//when clicked this will turn on definition
   });
-  $(".clickable").click(function() {
-    $(".showing").fadeToggle();
-    $(".hidden").fadeToggle();
+  $ ("#operators").click(function() {//jquery is attaching the click event handler to the specific ID
+    $("#operators .titleCard").toggle();//when clicked this will turn off titlecard
+    $("#operators .definition").toggle();//when clicked this will turn on defination
   });
-})
+
+
+});
